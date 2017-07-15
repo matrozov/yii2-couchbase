db66f09399d4a9e39b239730ad1e8a913a38b41f
Matrozov <oleg.matrozov@gmail.com>
2017-07-16 02:40:43 +0300

    WIP

--- tests/data/travis/couchbase-setup.sh
+++ tests/data/travis/couchbase-setup.sh
@@ -9,20 +9,17 @@ dpkg -i couchbase-release-1.0-2-amd64.deb
 rm couchbase-release-1.0-2-amd64.deb
 apt-get -y update
 apt-get -y install libcouchbase-dev build-essential php-dev zlib1g-dev
+
 pecl install pcs-1.3.3
 pecl install igbinary
 pecl install couchbase
+
 echo "extension=pcs.so" > /etc/php/7.0/mods-available/pcs.ini
-ln -s /etc/php/7.0/mods-available/pcs.ini /etc/php/7.0/fpm/conf.d/20-pcs.ini
 ln -s /etc/php/7.0/mods-available/pcs.ini /etc/php/7.0/cli/conf.d/20-pcs.ini
 echo "extension=igbinary.so" > /etc/php/7.0/mods-available/igbinary.ini
-ln -s /etc/php/7.0/mods-available/igbinary.ini /etc/php/7.0/fpm/conf.d/20-igbinary.ini
 ln -s /etc/php/7.0/mods-available/igbinary.ini /etc/php/7.0/cli/conf.d/20-igbinary.ini
 echo "extension=couchbase.so" > /etc/php/7.0/mods-available/couchbase.ini
-ln -s /etc/php/7.0/mods-available/couchbase.ini /etc/php/7.0/fpm/conf.d/30-couchbase.ini
 ln -s /etc/php/7.0/mods-available/couchbase.ini /etc/php/7.0/cli/conf.d/30-couchbase.ini
-
-service php7.0-fpm restart
 
 sleep 20
 
